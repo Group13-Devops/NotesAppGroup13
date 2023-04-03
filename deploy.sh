395d006f693418cdf3d9104c1d70293c1b329acc
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2023-04-03 15:59:39 +0100

    removed keys before re-adding them

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ cd NotesAppGroup13/
 #install dependencies
 npm install
 npm install ejs
+# Remove existing privatekey.pem and server.crt files
+rm -f privatekey.pem server.crt
+# Create privatekey.pem and server.crt from environment variables
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 #start the app
