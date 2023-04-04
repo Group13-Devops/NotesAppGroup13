362473492f2ea53b1c1cc6c4f08e60ad53584d15
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2023-04-04 22:14:08 +0100

    changed command in config

--- deploy.sh
+++ deploy.sh
@@ -9,22 +9,24 @@ sudo apt install -y nodejs npm
 sudo npm install -g pm2
 
 # Stop any running instance of the application
-#pm2 restart GroupThirteen
+pm2 delete GroupThirteen
 
 # Remove the old repository folder, if it exists
 rm -rf NotesAppGroup13/
+if [ $? -eq 0 ]; then
+  echo "Successfully removed old repository folder"
+else
+  echo "Failed to remove old repository folder"
+  exit 1
+fi
 
 # Clone the latest version of the repository
 git clone https://github.com/Group13-Devops/NotesAppGroup13.git
-
-#Pull most recent main branch 
-#git pull origin main
 
 # Change to the repository folder
 cd NotesAppGroup13/
 echo "Latest commit:"
 git log -1 --oneline
-
 
 # Install npm dependencies
 npm install
@@ -33,6 +35,5 @@ npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
-# Start (or restart) the application with pm2
+# Start the application with pm2
 pm2 start ./bin/www --name GroupThirteen
-#node app.js
